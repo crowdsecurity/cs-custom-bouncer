bd2ab1b3555dae96dbea856c715a9067bde19e9d
Marco Mariani <marco@crowdsec.net>
2023-02-23 13:29:53 +0100

    wip

---
+++ tests/stdinmode/conftest.py
@@ -0,0 +1,92 @@
+"""
+Full integration test with a real Crowdsec running in Docker
+"""
+
+import contextlib
+import os
+# import json
+import subprocess
+# import unittest
+import time
+from pathlib import Path
+# from time import sleep
+import psutil
+
+import pytest
+
+from tests.mock_lapi import MockLAPI
+# from tests.utils import generate_n_decisions
+
+SCRIPT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
+PROJECT_ROOT = SCRIPT_DIR.parent.parent
+BOUNCER_BINARY_PATH = PROJECT_ROOT.joinpath("crowdsec-custom-bouncer")
+# CUSTOM_BINARY_PATH = SCRIPT_DIR.joinpath("custombinary")
+CONFIG_PATH = SCRIPT_DIR.joinpath("crowdsec-custom-bouncer.yaml")
+
+# How long to wait for a child process to spawn
+CHILD_SPAWN_TIMEOUT = 1
+
+
+class BouncerProc:
+    def __init__(self, popen, outpath):
+        self.popen = popen
+        self.proc = psutil.Process(popen.pid)
+        self.outpath = outpath
+
+    def wait_for_child(self, timeout=CHILD_SPAWN_TIMEOUT):
+        start = time.monotonic()
+        while time.monotonic() - start < timeout:
+            children = self.proc.children()
+            if children:
+                return children[0]
+            time.sleep(0.1)
+        raise TimeoutError("No child process found")
+
+    def halt_children(self):
+        for child in self.proc.children():
+            child.kill()
+
+    def children(self):
+        return self.proc.children()
+
+    def get_output(self):
+        return pytest.LineMatcher(self.outpath.read_text().splitlines())
+
+    def wait_for_fnmatch_lines(self, lines, timeout=2):
+        # TODO: use watcher class from pytest-cs
+        time.sleep(timeout)
+        self.get_output().fnmatch_lines(lines)
+
+
+@pytest.fixture(scope='session')
+def bouncer(tmp_path_factory):
+    @contextlib.contextmanager
+    def closure():
+        # create stout and stderr files
+        outdir = tmp_path_factory.mktemp("output")
+        outpath = outdir / "output.txt"
+        with open(outpath, "w") as f:
+            cb = subprocess.Popen(
+                    [BOUNCER_BINARY_PATH, "-c", CONFIG_PATH],
+                    stdout=f,
+                    stderr=subprocess.STDOUT,
+                    )
+        try:
+            yield BouncerProc(cb, outpath)
+        finally:
+            cb.kill()
+            cb.wait()
+    return closure
+
+
+@pytest.fixture(scope='session')
+def lapi():
+    @contextlib.contextmanager
+    def closure():
+        lapi = MockLAPI()
+        lapi.start()
+        try:
+            yield lapi
+        finally:
+            lapi.stop()
+    return closure
---
+++ tests/stdinmode/test_custom_bouncer_refact.py
@@ -0,0 +1,108 @@
+"""
+Full integration test with a real Crowdsec running in Docker
+"""
+
+import json
+import time
+
+from tests.utils import generate_n_decisions
+
+import pytest
+
+# BOUNCER_BINARY_PATH = PROJECT_ROOT.joinpath("crowdsec-custom-bouncer")
+
+LAPI_CONNECT_TIMEOUT = 1
+
+
+def test_no_lapi(bouncer):
+    with bouncer() as cb:
+        time.sleep(LAPI_CONNECT_TIMEOUT)
+        cb.get_output().fnmatch_lines([
+            "*connection refused*",
+            "*terminating bouncer process*",
+        ])
+
+
+def test_binary_monitor(lapi, bouncer):
+    with lapi(), bouncer() as cb:
+        cb.wait_for_fnmatch_lines([
+            "*Using API key auth*",
+            "*Processing new and deleted decisions . . .*",
+        ])
+        child = cb.wait_for_child()
+        assert child.name() == "custombinary"
+        assert len(cb.children()) == 1
+
+        # Let's kill custombinary and see if it's restarted max_retry times (2)
+        cb.halt_children()
+        cb.wait_for_child()
+        assert len(cb.children()) == 1
+
+        cb.halt_children()
+        cb.wait_for_child()
+        assert len(cb.children()) == 1
+
+        # This will exceed max_retry and the bouncer would stop
+        cb.halt_children()
+        with pytest.raises(TimeoutError):
+            cb.wait_for_child()
+        assert len(cb.children()) == 0
+
+        # XXX: do we bother with wait() and poll()?
+
+
+def test_add_decisions(lapi, bouncer):
+    with lapi() as lp, bouncer() as cb:
+        cb.wait_for_fnmatch_lines([
+            "*Using API key auth*",
+            "*Processing new and deleted decisions . . .*",
+        ])
+        lp.ds.insert_decisions(generate_n_decisions(5))
+        time.sleep(0.5)
+        with open("data.txt") as f:
+            lines = f.readlines()
+        assert len(lines) == 5
+        for i, line in enumerate(lines):
+            line = json.loads(line)
+            assert line["id"] == i
+            assert line["action"] == "add"
+
+
+def test_cache_retention(lapi, bouncer):
+    with lapi() as lp, bouncer() as cb:
+        cb.wait_for_fnmatch_lines([
+            "*Using API key auth*",
+            "*Processing new and deleted decisions . . .*",
+        ])
+        decisions = generate_n_decisions(2)
+        lp.ds.insert_decisions(decisions)
+        time.sleep(1)
+        lp.ds.insert_decisions(decisions)
+        time.sleep(1)
+        with open("data.txt") as f:
+            assert len(f.readlines()) == 2
+
+
+def test_delete_decisions(lapi, bouncer):
+    with lapi() as lp, bouncer() as cb:
+        cb.wait_for_fnmatch_lines([
+            "*Using API key auth*",
+            "*Processing new and deleted decisions . . .*",
+        ])
+        decisions = generate_n_decisions(5)
+        lp.ds.insert_decisions(decisions)
+        decision_ids = list(map(lambda x: x["id"], decisions))
+        time.sleep(0.5)
+        list(map(lp.ds.delete_decision_by_id, decision_ids))
+        time.sleep(0.5)
+        with open("data.txt") as f:
+            lines = f.readlines()
+        assert len(lines) == 10
+        current_decisions = set()
+        for i, line in enumerate(lines):
+            line = json.loads(line)
+            if line["action"] == "add":
+                current_decisions.add(line["id"])
+            elif line["action"] == "del":
+                current_decisions.remove(line["id"])
+        assert len(current_decisions) == 0
