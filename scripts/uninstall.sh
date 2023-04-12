e121e1a65675746829f7d0cb5fe6737361f381a9
Marco Mariani <marco@crowdsec.net>
2023-04-12 09:51:39 +0200

    Cleanup packaging

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -22,21 +22,3 @@ uninstall() {
 uninstall
 msg succ "$BOUNCER has been successfully uninstalled"
 exit 0
-#!/usr/bin/env bash
-
-SYSTEMD_PATH_FILE="/etc/systemd/system/crowdsec-custom-bouncer.service"
-LOG_FILE="/var/log/crowdsec-custom-bouncer.log"
-CONFIG_DIR="/etc/crowdsec/bouncers"
-BIN_PATH_INSTALLED="/usr/local/bin/crowdsec-custom-bouncer"
-
-uninstall() {
-	systemctl stop crowdsec-custom-bouncer
-	rm -f "${CONFIG_DIR}/crowdsec-custom-bouncer.yaml"
-	rm -f "${SYSTEMD_PATH_FILE}"
-	rm -f "${BIN_PATH_INSTALLED}"
-	rm -f "${LOG_FILE}"
-}
-
-uninstall
-
-echo "crowdsec-custom-bouncer uninstall successfully"
--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -21,23 +21,3 @@ systemctl start "$SERVICE" || msg warn "$SERVICE failed to start, please check t
 
 msg succ "$BOUNCER upgraded successfully."
 exit 0
-#!/usr/bin/env bash
-BIN_PATH_INSTALLED="/usr/local/bin/crowdsec-custom-bouncer"
-BIN_PATH="./crowdsec-custom-bouncer"
-
-
-upgrade_bin() {
-    rm "${BIN_PATH_INSTALLED}" || (echo "crowdsec-custom-bouncer is not installed, exiting." && exit 1)
-    install -v -m 755 -D "${BIN_PATH}" "${BIN_PATH_INSTALLED}"
-}
-
-
-if ! [ $(id -u) = 0 ]; then
-    log_err "Please run the upgrade script as root or with sudo"
-    exit 1
-fi
-
-systemctl stop crowdsec-custom-bouncer
-upgrade_bin
-systemctl start crowdsec-custom-bouncer
-echo "crowdsec-custom-bouncer upgraded successfully."
