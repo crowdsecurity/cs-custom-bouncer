1bb49b0ccf032040cc47ae95b2063f34ad8081f3
Marco Mariani <marco@crowdsec.net>
2023-05-16 00:00:20 +0200

    main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+
+	"github.com/crowdsecurity/cs-custom-bouncer/cmd"
+)
+
+func main() {
+	err := cmd.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
