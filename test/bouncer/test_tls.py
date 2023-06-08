cf3a6fa1d6195c3c7d5fc9614a294f3f3a3d3a6a
Marco Mariani <marco@crowdsec.net>
2023-06-08 09:15:32 +0200

    check log on crowdsec side

--- test/bouncer/test_tls.py
+++ test/bouncer/test_tls.py
@@ -86,6 +86,8 @@ def test_tls_mutual(crowdsec, certs_dir, api_key_factory, bouncer, cb_stream_cfg
                 "*API error: access forbidden*",
             ])
 
+        cs.wait_for_log("*client certificate OU (?agent-ou?) doesn't match expected OU (?bouncer-ou?)*")
+
         cfg['cert_path'] = (certs / 'bouncer.crt').as_posix()
         cfg['key_path'] = (certs / 'bouncer.key').as_posix()
 
