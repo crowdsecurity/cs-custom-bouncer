f31240947b95288429df62d3afdf2bfd28623c30
marco <marco@crowdsec.net>
2025-03-28 12:04:50 +0100

    wip

--- test/tests/bouncer/test_tls.py
+++ test/tests/bouncer/test_tls.py
@@ -92,7 +92,7 @@ def test_tls_mutual(crowdsec, certs_dir, api_key_factory, bouncer, cb_stream_cfg
                 ]
             )
 
-        cs.wait_for_log("*client certificate OU (?agent-ou?) doesn't match expected OU (?bouncer-ou?)*")
+        cs.wait_for_log("*client certificate OU ?agent-ou? doesn't match expected OU ?bouncer-ou?*")
 
         cfg["cert_path"] = (certs / "bouncer.crt").as_posix()
         cfg["key_path"] = (certs / "bouncer.key").as_posix()
