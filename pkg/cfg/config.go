b17d64af6d826a2ff606ec4af8b6b0d5a8df7713
marco <marco@crowdsec.net>
2025-10-14 15:46:06 +0200

    update crowdsec dependencies

--- pkg/cfg/config.go
+++ pkg/cfg/config.go
@@ -10,7 +10,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
 
-	"github.com/crowdsecurity/go-cs-lib/yamlpatch"
+	"github.com/crowdsecurity/go-cs-lib/csyaml"
 )
 
 type PrometheusConfig struct {
@@ -39,7 +39,7 @@ type BouncerConfig struct {
 
 // MergedConfig() returns the byte content of the patched configuration file (with .yaml.local).
 func MergedConfig(configPath string) ([]byte, error) {
-	patcher := yamlpatch.NewPatcher(configPath, ".local")
+	patcher := csyaml.NewPatcher(configPath, ".local")
 	data, err := patcher.MergedPatchContent()
 	if err != nil {
 		return nil, err
